5b44e5a3ae1d4d255610c882a22a3e0d6530a87a
Okan Aslan <okannaslann@hotmail.com>
2022-04-15 01:19:08 +0300

    Initial Commit

---
+++ HaritaUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaritaUygulamasi
+//
+//  Created by MacBook on 15.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
