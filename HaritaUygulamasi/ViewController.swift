1ea8d4f60633a1c8125aa6ddc3fd4e7c80503f70
Okan Aslan <okannaslann@hotmail.com>
2022-04-16 18:46:23 +0300

    map can be opened

--- HaritaUygulamasi/ViewController.swift
+++ HaritaUygulamasi/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager() //burda locationManager objesi yaratıldı
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest //en iyi keskinliği sağlıyor. Bazen bu kadar keskinliğe gerek olmaz. Mesela 100m yakın kadarını buldursak yeterli olabilir. Gerekli ayarlamalar yapılır
+        
+        locationManager.requestWhenInUseAuthorization() //kullanıcı app'i kullanırken konum bilgisi alınır
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //print(locations[0].coordinate.latitude)
+        //print(locations[0].coordinate.longitude)
+        
+    
     }
 
 
